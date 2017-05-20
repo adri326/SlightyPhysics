da6847c759c68f615ff690f18515209a974a22f2
= <=>
2017-05-20 17:39:18 +0000

    Added some new additions

---
+++ constants.js
@@ -0,0 +1,7 @@
+const DIR = {
+  Xplus: 0,
+  Xminus: 1,
+  Yplus: 2,
+  Yminux: 3
+},
+BASECOLOR = "black"
--- engine.js
+++ engine.js
@@ -2,7 +2,7 @@
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
   window.requestAnimationFrame = requestAnimationFrame;
   })();
-  
+
 var canvas = document.getElementById("canvas"),
 ctx = canvas.getContext("2d"),
 width = 500,
@@ -12,43 +12,75 @@ player = {
     y: height-10,
     width: 10,
     height: 10,
-    speed: 3,
-    jumpForce: 1.7,
+    speed: 2.5,
+    jumpForce: 1.9,
     velX: 0,
     velY: 0,
-    jumping: false,
+    jumping: 0,
     grounded: false,
     color: "orange",
     walljump: true,
-    walljumpForceX: 2.2,
-    walljumpForceY: 1.4
+    walljumpForce: 1.5
 },
 keys = [],
 boxes = [],
 execute=[],
-friction = 0.8,
-gravity = 0.3;
+groundFriction = 0.6,
+airFriction = 0.975,
+walljumpForceX = 0.7,
+walljumpForceY = 1,
+gravity = 0.3,
+jumpControl = 10.0,
+jumpForce = 20,
+grid = {
+  sizeX: 10,
+  sizeY: 10
+};
 
 canvas.width = width;
 canvas.height = height;
 
-function addBox(sx, sy, xl, yl) {
+function getGridMaxX() {
+  return canvas.width / grid.sizeX - 1;
+}
+function getGridMaxY() {
+  return canvas.height / grid.sizeY - 1;
+}
+
+function addBox(sx, sy, xl, yl, color = BASECOLOR) {
     boxes.push({
         x: sx,
         y: sy,
         width: xl,
-        height: yl
+        height: yl,
+        color: color
     });
     return boxes.length-1;
 }
-function addExe(sx, sy, xl, yl, Code, COLOR) {
+function addGridBox(x, y, color = BASECOLOR) {
+  addBox(x * grid.sizeX, y * grid.sizeY, grid.sizeX, grid.sizeY, color);
+}
+function addGridBoxLine(x, y, dir, n, color = BASECOLOR) {
+  for (i = 0; i < n; i++) {
+    if (dir == DIR.Xminus) {
+      addGridBox(x - i, y, color);
+    } else if (dir == DIR.Xplus) {
+      addGridBox(x + i, y, color);
+    } else if (dir == DIR.Yminus) {
+      addGridBox(x, y - i, color);
+    } else if (dir == DIR.Yplus) {
+      addGridBox(x, y + i, color);
+    }
+  }
+}
+function addExe(sx, sy, xl, yl, Code, color = BASECOLOR) {
     execute.push({
         x: sx,
         y: sy,
         width: xl,
         height: yl,
         code: Code,
-        color: COLOR,
+        color: color,
     });
   return execute.length-1;
 }
@@ -64,53 +96,12 @@ function clearBox() {
 }
 
 clearBox();
-  
+
 function level1() {
   clearBox();
-  addBox(120, 480, 50, 5);
-  addBox(170, 460, 50, 5);
-  addBox(230, 430, 50, 5);
-  addBox(300, 400, 100, 5);
-  addBox(430, 370, 60, 5);
-  addBox(420, 340, 50, 5);
-  addBox(400, 310, 50, 5);
-  addBox(470, 310, 25, 5);
-  addBox(360, 170, 5, 100);
-  addBox(310, 170, 50, 5);
-  addExe(310, 150, 20, 20, function() {
-    addBox(250,170,30,5);
-  }, "yellow");
-  addExe(170,155,15,15, function() {
-    gravity=0.1
-  }, "blue");
-  addBox(170,170,30,5);
-  addBox(0,170,30,5);
-  addExe(10,155,15,15, function() {
-    level2();
-    gravity = 0.3;
-  }, "green");
-}
-function level2() {
-  clearBox();
-  addBox(200, 440, 100, 10);
-  addBox(150, 400, 40, 10);
-  addBox(310, 400, 40, 10);
-  addBox(120, 300, 10, 80);
-  addBox(380, 300, 10, 80);
-  addBox(200, 320, 100, 10);
-  addBox(245, 220, 10, 70);
-  addBox(80, 310, 40, 10);
-  addBox(390, 310, 40, 10);
-  addBox(310, 230, 40, 10);
-  addBox(150, 230, 40, 10);
-  addExe(320, 210, 20, 20, function() {
-    addExe(160, 210, 20, 20, function() {
-      end();
-    }, "green");
-  }, "yellow");
-}
-function end() {
-    alert('Demo finished!');
+  addGridBoxLine(30, getGridMaxY() - 1, DIR.Yminus, 5, "#8ec1f4");
+  addGridBoxLine(25, getGridMaxY() - 1, DIR.Yminus, 3, "#2552d9");
+  addGridBoxLine(20, getGridMaxY() - 1, DIR.Yminus, 1, "#0f228c");
 }
 
 function randBot() {
@@ -126,15 +117,18 @@ function randBot() {
         keys[k] = true;
     }, 600);
   }
-  
+
   function update() {
     // check keys
     if (keys[38] || keys[32]) { // up arrow or space
-      if (!player.jumping && player.grounded) {
-        player.jumping = true;
+      if (player.jumping < 1 && (player.grounded || player.jumping != 0)) {
+        player.jumping += 1 / jumpControl;
         player.grounded = false;
-        player.velY = -player.speed * player.jumpForce;
+        //player.velY = -player.speed * player.jumpForce / jumpControl;
+        player.velY = -jumpForce * player.jumpForce / jumpControl;
       }
+    } else {
+      player.jumping = 0;
     }
     if (keys[39]) {             // right arrow
       if (player.velX < player.speed) {
@@ -146,73 +140,77 @@ function randBot() {
         player.velX--;
       }
     }
-    
-    player.velX *= friction;
+
+    if (player.grounded) {
+      player.velX *= groundFriction;
+    }
+    player.velX *= airFriction;
     player.velY += gravity;
+    player.velY *= airFriction;
     player.grounded = false;
-    
+
     ctx.clearRect(0, 0, width, height);
-    
+
     for (var i = 0; i < boxes.length; i++) {
-        ctx.beginPath();
+        ctx.beginPath(); // TODO: remove
         ctx.fillStyle = "black";
         drawBox(boxes[i]);
-            
+
         var dir = colCheck(player, boxes[i]);
-        
+
         if (dir === "r") {
             player.velX = 0;
             if (keys[38] && player.walljump == true) {
-                player.velY = -player.speed * player.walljumpForceY;
-                player.velX = player.velX - player.speed * player.walljumpForceX;
+                player.velY = -player.speed * player.walljumpForce * walljumpForceY;
+                player.velX = player.velX - player.speed * player.walljumpForce * walljumpForceX;
            }
         } else if (dir === "l" ) {
             player.velX = 0;
             if (keys[38] && player.walljump == true) {
-                player.velY = -player.speed * player.walljumpForceY;
-                player.velX = player.velX + player.speed * player.walljumpForceX;
+                player.velY = -player.speed * player.walljumpForce * walljumpForceY;
+                player.velX = player.velX + player.speed * player.walljumpForce * walljumpForceX;
             }
         } else if (dir === "b") {
             player.grounded = true;
-            player.jumping = false;
+            player.jumping = 0;
         } else if (dir === "t") {
          player.velY *= -1;
         }
     }
-    
+
     for (var i = 0; i <  execute.length; i++) {
       ctx.beginPath();
       drawExe(execute[i]);
-          
+
       var dir = colCheck(player,  execute[i]);
-      
+
       if (dir === "r" || dir === "l" || dir === "b" || dir === "t") {
           var code = execute[i].code;
           execute.splice(i,1);
           code();
       }
     }
-    
+
     if(player.grounded) {
         player.velY = 0;
     }
-    
+
     player.x += player.velX;
     player.y += player.velY;
-    
+
     drawPlayer(player);
-    
+
     requestAnimationFrame(update);
 }
 
-    function drawBox(p1) {
-    ctx.fillStyle = "black";
+function drawBox(p1) {
+    ctx.fillStyle = p1.color;
     ctx.rect(p1.x, p1.y, p1.width, p1.height);
     ctx.fill();
     ctx.closePath();
 }
 function drawExe(p1) {
-    ctx.fillStyle =p1.color;
+    ctx.fillStyle = p1.color;
     ctx.rect(p1.x, p1.y, p1.width, p1.height);
     ctx.fill();
     ctx.closePath();
@@ -233,7 +231,7 @@ function colCheck(shapeA, shapeB) {
         hWidths = (shapeA.width / 2) + (shapeB.width / 2),
         hHeights = (shapeA.height / 2) + (shapeB.height / 2),
         colDir = null;
-    
+
     // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
     if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
         // figures out on which side we are colliding (top, bottom, left, or right)
